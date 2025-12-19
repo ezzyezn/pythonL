e2b6082b4206a5603dc0ababd97eabc883160aaf
ezzyezn <art.samerui@gmail.com>
2025-12-19 23:57:04 +0100

    Maximum-speed number checking, bug fixes, and adding a timer.

--- passsword.py
+++ passsword.py
@@ -1,20 +1,22 @@
-import random
-guessed = False
+import time
 attempts = 0
 while True:
     password = input("CAN I HACK YOU PASSWORD? GET ME YOUR PASSWORD:")
-    if len(password) != 4:
-        print("use only 4 numbers")
+    if len(password) != 7:
+        print("use only 7 numbers")
         continue
     if not password.isdigit():
         print("Password must contain ONLY digits!")
         continue
     break
-while not guessed:
+start_time = time.time()
+for i in range (10_000_000):
     attempts += 1
-
-    try_password = "".join(str(random.randint(0, 9)) for _ in range(4))
-    print(try_password)
+    try_password = str(i).zfill(7)
     if password == try_password:
-        print(f"Your Password ({try_password}) \n I used {attempts} attempts")
-        guessed = True
+        end_time = time.time()
+        elapsed = end_time - start_time
+        print(f"Your Password ({try_password}) \n"
+        f" I used {attempts} attempts \n"
+        f"Time spent {elapsed:.4f} seconds")
+        break
