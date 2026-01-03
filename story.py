5501abb9252134bbad567718521052c8a0228efe
ezzyezn <art.samerui@gmail.com>
2026-01-03 20:22:04 +0100

    Fixed little bugs and "exit"

--- story.py
+++ story.py
@@ -8,20 +8,21 @@ while game:
     if nextw == "":
         print("Choose your work:", work[:-1])
         chose = input()
-        print("your work is a ", chose)
-        # ----- MINER -----
-        if chose == work[0]:
-            money += work[-1][0]
-        # ----- CURIER -----
-        elif chose == work[1]:
-            money += work[-1][1]
-        # ----- PROGRAMER -----
-        elif chose == work[2]:
-            money += work[-1][2]
-        # ----- TRASH -----
-        elif chose == work[3]:
-            money += work[-1][3]
+        if chose != work[4]:
+            print("your work is a ", chose)
+            # ----- MINER -----
+            if chose == work[0]:
+                money += work[-1][0]
+            # ----- CURIER -----
+            elif chose == work[1]:
+                money += work[-1][1]
+            # ----- PROGRAMER -----
+            elif chose == work[2]:
+                money += work[-1][2]
+            # ----- TRASH -----
+            elif chose == work[3]:
+                money += work[-1][3]
+            print("You have: ", money, "$")
         # ----- EXIT -----
-        elif chose == work[4]:
+        if chose == work[4]:
             game = work[-1][4]
-    print("You have: ", money, "$")
