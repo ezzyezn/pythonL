02c11d552e7ef7fad3794cd6b1918e8843501d7e
ezzyezn <art.samerui@gmail.com>
2026-01-14 23:24:36 +0100

    Added game buttons

--- gameOfNumberGui.py
+++ gameOfNumberGui.py
@@ -14,11 +14,35 @@ class Stats:
         self.first_number = 0
         self.last_number = 100
 stats = Stats()
+
 class GuiHuman:
     def __init__(self):
         self.label = Label(root, text="Think of number from 0 to 100",
                            bg="#FF7C7C", font=('Verdana',15))
 gui_human = GuiHuman()
+class GameButtons:
+    def __init__(self):
+        self.response = None
+
+        self.btn_more = Button(root, text="More",
+                             font=('Verdana', 12), bg='#6AE1A9',command=lambda: self.more())
+        self.btn_less = Button(root, text="Less",
+                             font=('Verdana', 12), bg='#6AE1A9',command=lambda: self.less())
+        self.btn_correct = Button(root, text="Correct",
+                             font=('Verdana', 12), bg='#6AE1A9',command=lambda: self.correct())
+    def more(self):
+        self.response = ">"
+        human_step()
+    def less(self):
+        self.response = "<"
+        human_step()
+    def correct(self):
+        self.response = "="
+gameBtn = GameButtons()
+class HumanWin:
+    def __init__(self):
+        self.label = Label(root, text=f"Computer guessed, your number is a {computer_number} \n Attempts were used: {attempts}",
+                     bg="#FF7C7C", font=('Verdana',12))
 #------------MAIN WINDOW------------
 root.geometry("500x500")
 root.resizable(width = False, height = False)
@@ -53,7 +77,23 @@ def human_step():
 
         gui_human.label.config(text=f"Computer thinks: {computer_number}")
 
+        gameBtn.btn_more.pack(pady=10)
+        gameBtn.btn_less.pack(pady=10)
+        gameBtn.btn_correct.pack(pady=10)
 
+        if gameBtn.response is None:
+            return
+
+        if gameBtn.response == ">":
+            stats.first_number = computer_number + 1
+        elif gameBtn.response == "<":
+            stats.last_number = computer_number - 1
+        elif gameBtn.response == "=":
+            wins()
+        gameBtn.response = None
+        human_step()
+    else:
+        print("you stupid")
 def game_human():
 
     label_choose.pack_forget()
